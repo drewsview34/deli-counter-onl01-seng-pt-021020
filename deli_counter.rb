9b3d6dc0ec24efbe23c27b6b2b1b2d46225aec1d
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 04:28:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,24 +4,16 @@ require 'pry'
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  elsif array.length == 3
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   else 
-    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+    new_array = array.each_with_index.map do |name, index| "#{index + 1}. #{name}"
+  end
+  puts "The line is currently: #{new_array.join(" ")}"
   end
 end
 
-def take_a_number(array, string)
-  counter = 0
-  if array.length == 0
-    array.unshift(string)
-  else
-    array.push(string)
-    puts "Welcome, #{string}. You are number #{array.length} in line."
-  end
-    katz_deli.each do |name|
-    puts "Welcome, #{name}. You are number #{counter += 1} in line."
-  end
+def take_a_number(katz, not_katz)
+    katz << not_katz
+    puts "Welcome, #{not_katz}. You are number #{katz.length} in line."
 end
 
 
@@ -29,8 +21,6 @@ def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    array.first
-    puts "Currently serving #{array.first}."
-    array.shift
+    puts "Currently serving #{array.shift}."
   end
 end
