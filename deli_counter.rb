15ead3304b0bfc06f9dcf6a83c980099f6a97700
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 04:19:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,6 @@ require 'pry'
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  elsif array.length == 3
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   else 
     puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
   end
